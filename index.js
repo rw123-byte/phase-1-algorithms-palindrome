aa43f5abc1c4c56baa6b85db34226b4d943984ce
ruthwekesa <wekesaruth569@gmail.com>
2022-09-03 17:33:51 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    const reversedWord = reverse(word)
+    if (word !== reversedWord) {
+        return false
+    }
+    return true
 }
 
-/* 
-  Add your pseudocode here
-*/
+function reverse(word) {
+    let newsplit = word.split('')
+    let newreverse = newsplit.reverse()
+    let reverseSplit = newreverse.join('')
+    return reverseSplit
+}
+// console.log(reversing('Abdi'))
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+
+    console.log("");
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
